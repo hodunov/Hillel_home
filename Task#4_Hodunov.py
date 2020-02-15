3ee633df8158dc1b316d91093d35dfd07dedb250
Artem Hodunov <hodunov.art@gmail.com>
2020-02-15 13:53:14 +0200

    Update all files

--- Task#4_Hodunov.py
+++
@@ -1,9 +0,0 @@
-print("Printing inclusive range")
-start = 20
-stop  = 30
-step  = 2
-# to get inclusive range change stop as stop+step
-stop +=step #now stop is 
-
-for i in range(start, stop, step):
-    print(i, end=', ')
