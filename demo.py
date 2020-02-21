118b9be81c5801f8f23bd923da5a40a8a82f5d4d
Artem Hodunov <hodunov.art@gmail.com>
2020-02-21 17:41:49 +0200

    some remarks

--- demo.py
+++ demo.py
@@ -1,1 +1,15 @@
-rrr
+
+user_list=[]
+
+
+
+while True:
+    user_month = int(input('Enter your month  '))
+    if 0 < user_month < 13:
+        user_list.append(user_month)
+        break
+    else:
+        print('LOL. Try again')
+
+
+user_date = int(input('Enter your year  '))
