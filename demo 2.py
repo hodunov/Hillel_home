657bb4709af5cca8533b37f75936c4ce0dcfe7f9
artgo_HP <art.godunov@outlook.com>
2020-02-28 22:01:12 +0200

    json

--- demo 2.py
+++ demo 2.py
@@ -2,21 +2,8 @@ import json
 
 with open('questions.json', 'r') as file:
     json_data = json.load(file)
-    for item in json_data:
-        if item["answer"]:
-            item[""] = "ARTEM"
-with open('questions.json', 'w') as file:
-    json.dump(json_data, file, indent=2)
-
-
-# Test
-
-import json
-
-with open('/path/to/josn_file.json', 'r') as file:
-    json_data = json.load(file)
-    for item in json_data:
-        if item['ParameterKey'] in ["Shell", "Type"]:
-            item['ParameterKey'] = "new value"
-with open('/path/to/josn_file.json', 'w') as file:
-    json.dump(json_data, file, indent=2)
+    for each in json_data['questions']:
+        print(each["q"])
+        each["answer"] = str(input("Your answer- "))
+    with open('questions.json', 'w') as f:
+        json.dump(json_data, f, indent=2)
