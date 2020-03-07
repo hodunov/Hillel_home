b1df2a1b0b309c469c55b20a5897f426dec88c1e
artgo_HP <art.godunov@outlook.com>
2020-03-07 22:20:03 +0200

    Classes first iteration

--- 19_Hodunov_classes.py
+++ 19_Hodunov_classes.py
@@ -2,84 +2,96 @@ class Hero:
     def __init__(self, name):
         self.name = name
 
+    def talk(self):
+        return print("-I'll eat you, Kolobok")
+
 
 class Grandpa(Hero):
     def beg_grendma(self):
-        return 'Dear grandma, I\'m hungry! =( Please bake a kolobok!'
+        return print('-Dear grandma, I\'m hungry! =( Please bake a kolobok!')
 
 
 class Grandma(Hero):
     def bake_kolobok(self):
-        return Kolobok('Kolobok is created')
+        print("~~~ Grandma Bake Kolobok ~~~ \n~~~ Kolobok created ~~~")
+        return Kolobok('Kolobok')
 
 
 class Kolobok(Hero):
     def rest(self):
-        return 'Kolobok rests'
+        return print('~~~ Kolobok rests ~~~')
 
     def roll(self):
-        return 'Kolobok rolls'
+        return print('~~~ Kolobok rolls ~~~')
 
     def sing_song(self):
-        return "HO HO HO, I'LL GO"
+        return print("-HO HO HO, I'LL GO")
 
     def die(self):
-        return "Kolobok died ='("
+        return print("~~~ Kolobok died ='( ~~~")
 
 
 class Bunny(Hero):
-    def talk(self):
-        return "I'll eat you, Kolobok"
+    pass
 
 
 class Wolf(Hero):
-    def talk(self):
-        return "I'll eat you, Kolobok"
+    pass
 
 
 class Fox(Hero):
-    def talk(self):
-        return "I'll eat you, Kolobok"
-
     def praise(self):
-        return "You're a good singer!"
+        return print("You're a good singer!")
 
     def request(self):
-        return "I can't hear well, please sit on my tongue and sing again."
+        return print("I can't hear well, please sit on my tongue and sing again.")
 
 
 class Tale:
-    def __init__(self, Grandpa, Grandma, Kolobok, Bunny, Wolf, Fox):
-        self.grandpa = Grandpa
-        self.grandma = Grandma
-        self.kolobok = Kolobok
-        self.bunny = Bunny
-        self.wolf = Wolf
-        self.fox = Fox
+    def __init__(self, Grandpa, Grandma, Bunny, Wolf, Fox):
+        self.Grandpa = Grandpa
+        self.Grandma = Grandma
+        self.Kolobok = None
+        self.Bunny = Bunny
+        self.Wolf = Wolf
+        self.Fox = Fox
 
     def grandmas_house(self):
-        self.grandpa = self.grandpa.beg_grendma()
-        self.kolobok = self.grandma.bake_kolobok()
-        self.kolobok = self.kolobok.rest()
+        print("|||Grandma's house|||\nGrandpa says:")
+        self.Grandpa.beg_grendma()
+        self.Kolobok = self.Grandma.bake_kolobok()
+        self.Kolobok.rest()
 
     def wood(self):
-        self.kolobok = self.kolobok.roll()
-        self.bunny = self.bunny.talk()
-        self.kolobok = self.kolobok.sing_song()
-        self.kolobok = self.kolobok.roll()
-        self.wolf = self.wolf.talk()
-        self.kolobok = self.kolobok.sing_song()
-        self.kolobok = self.kolobok.roll()
-        self.fox = self.fox.talk()
-        self.kolobok = self.kolobok.sing_song()
-        self.fox = self.fox.praise()
-        self.fox = self.fox.request()
-        self.kolobok = self.kolobok.sing_song()
-        self.kolobok = self.kolobok.die()
+        print("|||Wood|||")
+        self.Kolobok.roll()
+        print("Bunny says:")
+        self.Bunny.talk()
+        print("Kolobok says:")
+        self.Kolobok.sing_song()
+        self.Kolobok.roll()
+        print("Wolf says:")
+        self.Wolf.talk()
+        print("Kolobok says:")
+        self.Kolobok.sing_song()
+        self.Kolobok.roll()
+        print("Fox says:")
+        self.Fox.talk()
+        print("Kolobok says:")
+        self.Kolobok.sing_song()
+        print("Fox says:")
+        self.Fox.praise()
+        self.Fox.request()
+        print("Kolobok says:")
+        self.Kolobok.sing_song()
+        self.Kolobok.die()
 
     def start(self):
         self.grandmas_house()
         self.wood()
 
 
-Tale.start()
+Grandpa_1, Grandma_1, Bunny_1, Wolf_1, Fox_1 = Grandpa('Ded'), Grandma('Babka'), Bunny("Bunny"), Wolf('Wolk'), Fox(
+    'Lisa'),
+
+Tale(Grandpa_1, Grandma_1, Bunny_1, Wolf_1, Fox_1).start()
--- demo.py
+++ demo.py
@@ -4,25 +4,37 @@ class Hero:
 
 
 class Colobok(Hero):
-    pass
+    def roll(self):
+        return print('Kolobok rolls')
+
+    def rest(self):
+        return print('Kolobok rests')
+
 
 
 class Babka(Hero):
     def bake_colobok(self):
+        print('Babka bake Kolobok')
         return Colobok('Kolobok')
 
 
 class Tale:
-    def __init__(self, babka, ded):
-        self.babka = babka
-        self.ded = ded
-        self.colobok = None
+    def __init__(self, Babka):
+        self.Babka = Babka
+        self.Colobok = None
 
     def babkin_dom(self):
-        self.colobok = self.babka.bake_colobok()
+        self.Colobok = self.Babka.bake_colobok()
+        self.Colobok.roll()
+        self.Colobok.rest()
+
+    def end(self):
+        return print('end of tail')
 
     def start(self):
         self.babkin_dom()
+        self.end()
 
 
-Tale.start(self)
+babushka = Babka('Babushka')
+Tale(babushka).start()
