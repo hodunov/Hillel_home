0a9dad3ae3cec4a462d0148a09936d6fefd2fd0f
artgo_HP <art.godunov@outlook.com>
2020-03-07 22:57:30 +0200

    renaming

--- 19_Hodunov_classes.py
+++ 19_Hodunov_classes.py
@@ -1,54 +1,73 @@
 class Hero:
+    """description of all heroes"""
     def __init__(self, name):
+        """Names of all heroes"""
         self.name = name
 
     def talk(self):
+        """Speech of bad heroes"""
         return print("-I'll eat you, Kolobok")
 
 
 class Grandpa(Hero):
+    """Grandpa info"""
     def beg_grendma(self):
+        """Speech to Grandma"""
         return print('-Dear grandma, I\'m hungry! =( Please bake a kolobok!')
 
 
 class Grandma(Hero):
+    """Grandma info"""
     def bake_kolobok(self):
+        """Baking"""
         print("~~~ Grandma Bake Kolobok ~~~ \n~~~ Kolobok created ~~~")
         return Kolobok('Kolobok')
 
 
 class Kolobok(Hero):
+    """Grandma info"""
     def rest(self):
+        """Func for rest"""
         return print('~~~ Kolobok rests ~~~')
 
     def roll(self):
+        """Func for roll"""
         return print('~~~ Kolobok rolls ~~~')
 
     def sing_song(self):
+        """Func for sing"""
         return print("-HO HO HO, I'LL GO")
 
     def die(self):
+        """Func for die"""
         return print("~~~ Kolobok died ='( ~~~")
 
 
 class Bunny(Hero):
+    """Bunny info"""
     pass
 
 
 class Wolf(Hero):
+    """Wolf info"""
     pass
 
 
 class Fox(Hero):
+    """Fox info"""
     def praise(self):
+        """First speech"""
         return print("You're a good singer!")
 
     def request(self):
-        return print("I can't hear well, please sit on my tongue and sing again.")
+        """Second speech"""
+        return print("I can't hear well, "
+                     "please sit on my tongue and sing again.")
 
 
 class Tale:
     def __init__(self, Grandpa, Grandma, Bunny, Wolf, Fox):
+        """Names of all heroes"""
         self.grandpa = Grandpa
         self.grandma = Grandma
         self.kolobok = None
@@ -57,12 +76,14 @@ class Tale:
         self.fox = Fox
 
     def grandmas_house(self):
+        """Story begin"""
         print("|||Grandma's house|||\nGrandpa says:")
         self.grandpa.beg_grendma()
         self.kolobok = self.grandma.bake_kolobok()
         self.kolobok.rest()
 
     def wood(self):
+        """Story in wood"""
         print("|||Wood|||")
         self.kolobok.roll()
         print("Bunny says:")
@@ -87,11 +108,12 @@ class Tale:
         self.kolobok.die()
 
     def start(self):
+        """Main story"""
         self.grandmas_house()
         self.wood()
 
 
-Grandpa_1, Grandma_1, Bunny_1, Wolf_1, Fox_1 = Grandpa('Ded'), Grandma('Babka'), Bunny("Bunny"), Wolf('Wolk'), Fox(
-    'Lisa'),
+GRANDPA, GRANDMA = Grandpa('Ded'), Grandma('Babka')
+BUNNY, WOLF, FOX = Bunny("Bunny"), Wolf('Wolk'), Fox('Lisa')
 
-Tale(Grandpa_1, Grandma_1, Bunny_1, Wolf_1, Fox_1).start()
+Tale(GRANDPA, GRANDMA, BUNNY, WOLF, FOX).start()
