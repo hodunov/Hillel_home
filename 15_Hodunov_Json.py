194c43dad4ac9f0e6ff89ac3a0a8fa37fbe98cc7
Artem Hodunov <hodunov.art@gmail.com>
2020-03-13 13:20:32 +0200

    rename

--- 15_Hodunov_Json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('questions.json', 'r') as file:
-    json_data = json.load(file)
-    for each in json_data['questions']:
-        print(each["q"])
-        each["answer"] = str(input("Your answer- "))
-    with open('questions.json', 'w') as f:
-        json.dump(json_data, f, indent=2)
---
+++ 20_Shapes.py
@@ -0,0 +1,32 @@
+"""
+Реализовать класс фигуры. На основе фигуры реализовать класс треугольника, квадрата и прямоугольника с методами
+подсчета площади и периметра. Методы должны возвращать значение, а не принтить
+
+Triangle
+square
+rectangle
+"""
+
+
+class Shape:
+    """all shapes"""
+    def __init__(self, a, b, c, h):
+        """All names"""
+        self.a = a
+        self.b = b
+        self.c = c
+        self.h = h
+
+
+class Triangle:
+    def __mul_(a,h):
+        Square = (a * h) / 2
+        return Square
+
+    def perimeter(a,b,c):
+        P = a + b +c
+        return P
+
+a = 12
+h= 3
+print(Triangle.__m
