3e894534a6064a1a26110653d2fc053e6deea4a6
Artem Hodunov <hodunov.art@gmail.com>
2020-03-29 16:14:25 +0300

    Code with errors and questions

--- hw/home_work.py
+++ hw/home_work.py
@@ -40,15 +40,34 @@ class Animal:
 
 
 class Mammal:
-    pass
+    """
+    У млекопитающий (Mammal) есть потомство (число)
+    """
+
+    def __init__(self, progeny):
+        self.progeny = progeny
 
 
 class Reptile:
-    pass
+    """
+    У рептилий (Reptile) - является ли ядовитым? (число 1 - Да, 0 - Нет)
+    """
+
+    def __init__(self, toxic):
+        self.toxic = toxic
 
 
 class Bird:
-    pass
+    """
+    У птиц (Bird):
+    - размах крыльев (число)
+    - умеет ли издавать звуки? (число 1 - Да, 0 - Нет)
+    - если умеет издавать звуки, то какие? (строка)
+    """
+    def __init__(self, wingspan, make_sounds, sounds=None):
+        self.wingspan = wingspan
+        self.make_sounds = make_sounds
+        self.sounds = sounds
 
 
 class AnimalDataBase:
@@ -63,8 +82,16 @@ class AnimalDataBase:
             self.save_animal_to_db(animal)
 
     def _create_animal(self, record):
+        global animal_to_db
         animal_args = record.split(' ')
-        return Animal(animal_args[0], animal_args[1], animal_args[2], animal_args[3])
+        if animal_args[1] == "Mammal":
+            animal_to_db = Animal(animal_args[0], Mammal(animal_args[4]), animal_args[2], animal_args[3])
+        if animal_args[1] == 'Reptile':
+            animal_to_db = Animal(animal_args[0], Reptile(animal_args[4]), animal_args[2], animal_args[3])
+        if animal_args[1] == 'Bird':
+            animal_to_db = Animal(animal_args[0], Bird(animal_args[4], animal_args[5]), animal_args[2],
+                                  animal_args[3])
+        return animal_to_db
 
 
 records = [
@@ -78,15 +105,35 @@ records = [
 
 db = AnimalDataBase()
 db.save_animals_from_records(records)
-print(db._AnimalDataBase__animal_db)
+print(db._AnimalDataBase__animal_db)  # Возвращаю объекты, а нужна строка?
 
-query_choice = """
-Query animal 
-[s] species
-[m] mass
-[l] ...
-[v] ...
-[w] ...
-[t] ...
-"""
-print(query_choice)
+# ______________________ 2  ____________________________
+
+query_choice = "[T] animal_type \n[SP] species \n[M] Mass \n" \
+               "[P] Progeny \n[T] Toxic \n[W] Wingspan \n[MS] Make_sounds \n[S] Sounds "
+
+while True:
+    chose_animal = input("Enter animal name here \n")
+    if chose_animal in db._AnimalDataBase__animal_db:  # как теперь пройтись по листу? так же через split?
+        print(query_choice)
+        input_property = input("Input letter of the property")
+        if input_property == "T":  # Можно через словарь релизовать? что-то вроде {"W": _AnimalDataBase__animal_db[1]}
+            print("Animal type is ")  # через split доставать значение?
+        if input_property == "SP":
+            print("Animal species is ")
+        if input_property == "M":
+            print("Animal mass is ")
+        if input_property == "P":
+            print("Animal progeny is ")
+        if input_property == "T":
+            print("")
+        if input_property == "W":
+            print("")
+        if input_property == "MS":
+            print("")
+        if input_property == "S":
+            print("")
+        else:
+            print("an animal does not have that attribute.")
+    else:
+        print("There's no such animal.")
